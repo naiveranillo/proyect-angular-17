ab18be21bf2c65b7f4c34fd1f2342e43e9ce23aa
Naiver Anillo <anillonaiver2a@gmail.com>
2023-11-27 18:05:14 -0500

    first commit

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,58 @@
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { Task } from '../../models/task.model';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  tasks = signal<Task[]>([
+    {
+      id: Date.now(),
+      title: 'Crear Proyecto',
+      completed: false
+    },
+    {
+      id: Date.now(),
+      title: 'Crear Componentes',
+      completed: false
+    },
+  ]);
+
+  changeHandler(event: Event){
+    const input = event.target as HTMLInputElement;
+    const newTask = input.value;
+    this.addTask(newTask);
+  }
+
+  addTask(title: string){
+    const newTask: Task = {
+      id: Date.now(),
+      title,
+      completed: false,
+    };
+
+    this.tasks.update((tasks) => [...tasks, newTask]);
+  }
+
+  deleteTask(index: number){
+    this.tasks.update((tasks) => tasks.filter((task, position) => position !== index));
+  }
+
+  taskCompletion(index: number){
+    this.tasks.update((tasks) => 
+      tasks.map((task, position) => {
+        if(position === index){
+          task.completed = !task.completed;
+        }
+
+        return task;
+      })
+    );
+  }
+}
---
+++ src/app/pages/labs/labs.component.ts
@@ -0,0 +1,46 @@
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-labs',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './labs.component.html',
+  styleUrl: './labs.component.css'
+})
+export class LabsComponent {
+  title = 'todo-app';
+  welcome = 'Bienvenido a mi primera aplicacion con Angular';
+  tasks = signal([
+    'Instalar el Angular CLI',
+    'Crear proyecto',
+    'Crear componentes',
+    'Crear servicio'
+  ]);
+
+  name = signal('Naiver');
+  age = 23;
+  disabled = true;
+  img = 'https://w3schools.com/howto/img_avatar.png';
+
+  person = {
+    name: 'Naiver',
+    age: 23,
+    avatar: this.img
+  }
+
+  clickHandler(){
+    alert('Hola');
+  }
+
+  changeHandler(event: Event){
+    const input = event.target as HTMLInputElement;
+    const newValue = input.value;
+    this.name.set(newValue);
+  }
+
+  keydownHandler(event: KeyboardEvent){
+    const input = event.target as HTMLInputElement;
+    console.log(input.value);
+  }
+}
